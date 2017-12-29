b8f12797af4d6a43114e0b4e6a5194b09614ad7b
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2017-12-29 08:58:02 -0500

    Added backend/frontend dependencies

--- backend/server.go
+++ backend/server.go
@@ -5,12 +5,15 @@ import "github.com/kataras/iris"
 func main() {
 	app := iris.New()
 
-	app.Get("/", func(c iris.Context) {
+	app.RegisterView(iris.HTML("./pages", ".html").Reload(true))
+	app.StaticWeb("/statics", "./statics")
 
+	app.Get("/", func(c iris.Context) {
+		c.View("index.html")
 	})
 	app.Post("/eval_photo", func(c iris.Context) {
 
 	})
 
-	app.Run(iris.Addr("3600"))
+	app.Run(iris.Addr(":3600"))
 }
